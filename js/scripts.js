91438f0d2871cb55b64fcdf55754d25a957bd1df
PaoloIncollu <paoloinco@gmail.com>
2024-07-25 14:02:10 +0200

    completato step 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,50 @@
 
-console.log('java collegato')
+/*
+
+step 
+
+1. creare un array con le informazioni
+2. stampare in console per ogni membro le informazioni
+3. stampare le informazioni sul dom 
+*/
+
+//step 1
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        name:'Angela Caroll',
+        role:'Chief Editor',
+        image:'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name:'Walter Gordon',
+        role:'Office Manager',
+        image:'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name:'Angela Lopez',
+        role:'Social Media Manager',
+        image:'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name:'Scott Estrada',
+        role:'Developer',
+        image:'scott-estrada-developer.jpg',
+    },
+
+    {
+        name:'Barbara Ramos',
+        role:'Graphic Designer',
+        image:'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+//console.log (team);
